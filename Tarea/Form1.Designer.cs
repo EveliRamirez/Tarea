c059d8c2c4417e8d6b4335a6b52b1742ca7d2853
EveliRamirez <152911713+EveliRamirez@users.noreply.github.com>
2024-02-09 12:07:09 -0600

    Add project files.

---
+++ Tarea/Form1.Designer.cs
@@ -0,0 +1,282 @@
+﻿namespace Tarea
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lienzo = new System.Windows.Forms.PictureBox();
+            this.picBlack = new System.Windows.Forms.PictureBox();
+            this.picBox = new System.Windows.Forms.PictureBox();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnPincel = new System.Windows.Forms.Button();
+            this.btnBorrar = new System.Windows.Forms.Button();
+            this.trackBartampincel = new System.Windows.Forms.TrackBar();
+            this.txtG = new System.Windows.Forms.TextBox();
+            this.txtR = new System.Windows.Forms.TextBox();
+            this.txtB = new System.Windows.Forms.TextBox();
+            this.btncirculo = new System.Windows.Forms.Button();
+            this.btncuadrado = new System.Windows.Forms.Button();
+            this.btntriangulo = new System.Windows.Forms.Button();
+            this.txtTamanoPincel = new System.Windows.Forms.TextBox();
+            this.btnCargarImagen = new System.Windows.Forms.Button();
+            this.btnRellenado = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.lienzo)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBlack)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBartampincel)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lienzo
+            // 
+            this.lienzo.BackColor = System.Drawing.SystemColors.Window;
+            this.lienzo.Location = new System.Drawing.Point(456, 67);
+            this.lienzo.Name = "lienzo";
+            this.lienzo.Size = new System.Drawing.Size(921, 488);
+            this.lienzo.TabIndex = 0;
+            this.lienzo.TabStop = false;
+            this.lienzo.Click += new System.EventHandler(this.lienzo_Click);
+            this.lienzo.MouseDown += new System.Windows.Forms.MouseEventHandler(this.lienzo_MouseDown);
+            this.lienzo.MouseMove += new System.Windows.Forms.MouseEventHandler(this.lienzo_MouseMove);
+            this.lienzo.MouseUp += new System.Windows.Forms.MouseEventHandler(this.lienzo_MouseUp);
+            // 
+            // picBlack
+            // 
+            this.picBlack.BackColor = System.Drawing.Color.Black;
+            this.picBlack.Location = new System.Drawing.Point(314, 178);
+            this.picBlack.Name = "picBlack";
+            this.picBlack.Size = new System.Drawing.Size(31, 29);
+            this.picBlack.TabIndex = 1;
+            this.picBlack.TabStop = false;
+            this.picBlack.Click += new System.EventHandler(this.picBlack_Click);
+            // 
+            // picBox
+            // 
+            this.picBox.BackColor = System.Drawing.Color.Red;
+            this.picBox.Location = new System.Drawing.Point(380, 178);
+            this.picBox.Name = "picBox";
+            this.picBox.Size = new System.Drawing.Size(34, 29);
+            this.picBox.TabIndex = 2;
+            this.picBox.TabStop = false;
+            this.picBox.Click += new System.EventHandler(this.picBox_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(271, 73);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(15, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "R";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(272, 149);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(14, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "B";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(271, 113);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(15, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "G";
+            // 
+            // btnPincel
+            // 
+            this.btnPincel.Location = new System.Drawing.Point(56, 73);
+            this.btnPincel.Name = "btnPincel";
+            this.btnPincel.Size = new System.Drawing.Size(75, 23);
+            this.btnPincel.TabIndex = 6;
+            this.btnPincel.Text = "pincel";
+            this.btnPincel.UseVisualStyleBackColor = true;
+            this.btnPincel.Click += new System.EventHandler(this.btnPincel_Click);
+            // 
+            // btnBorrar
+            // 
+            this.btnBorrar.Location = new System.Drawing.Point(56, 103);
+            this.btnBorrar.Name = "btnBorrar";
+            this.btnBorrar.Size = new System.Drawing.Size(75, 23);
+            this.btnBorrar.TabIndex = 7;
+            this.btnBorrar.Text = "borrar";
+            this.btnBorrar.UseVisualStyleBackColor = true;
+            this.btnBorrar.Click += new System.EventHandler(this.btnBorrar_Click);
+            // 
+            // trackBartampincel
+            // 
+            this.trackBartampincel.Location = new System.Drawing.Point(171, 67);
+            this.trackBartampincel.Name = "trackBartampincel";
+            this.trackBartampincel.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.trackBartampincel.Size = new System.Drawing.Size(45, 140);
+            this.trackBartampincel.TabIndex = 8;
+            // 
+            // txtG
+            // 
+            this.txtG.Location = new System.Drawing.Point(314, 106);
+            this.txtG.Name = "txtG";
+            this.txtG.Size = new System.Drawing.Size(100, 20);
+            this.txtG.TabIndex = 9;
+            this.txtG.Text = "0";
+            // 
+            // txtR
+            // 
+            this.txtR.Location = new System.Drawing.Point(314, 66);
+            this.txtR.Name = "txtR";
+            this.txtR.Size = new System.Drawing.Size(100, 20);
+            this.txtR.TabIndex = 10;
+            this.txtR.Text = "0";
+            // 
+            // txtB
+            // 
+            this.txtB.Location = new System.Drawing.Point(314, 142);
+            this.txtB.Name = "txtB";
+            this.txtB.Size = new System.Drawing.Size(100, 20);
+            this.txtB.TabIndex = 11;
+            this.txtB.Text = "0";
+            // 
+            // btncirculo
+            // 
+            this.btncirculo.Location = new System.Drawing.Point(56, 243);
+            this.btncirculo.Name = "btncirculo";
+            this.btncirculo.Size = new System.Drawing.Size(75, 23);
+            this.btncirculo.TabIndex = 12;
+            this.btncirculo.Text = "circulo";
+            this.btncirculo.UseVisualStyleBackColor = true;
+            this.btncirculo.Click += new System.EventHandler(this.btncirculo_Click);
+            // 
+            // btncuadrado
+            // 
+            this.btncuadrado.Location = new System.Drawing.Point(56, 214);
+            this.btncuadrado.Name = "btncuadrado";
+            this.btncuadrado.Size = new System.Drawing.Size(75, 23);
+            this.btncuadrado.TabIndex = 13;
+            this.btncuadrado.Text = "cuadrado";
+            this.btncuadrado.UseVisualStyleBackColor = true;
+            this.btncuadrado.Click += new System.EventHandler(this.btncuadrado_Click);
+            // 
+            // btntriangulo
+            // 
+            this.btntriangulo.Location = new System.Drawing.Point(56, 272);
+            this.btntriangulo.Name = "btntriangulo";
+            this.btntriangulo.Size = new System.Drawing.Size(75, 23);
+            this.btntriangulo.TabIndex = 14;
+            this.btntriangulo.Text = "triangulo";
+            this.btntriangulo.UseVisualStyleBackColor = true;
+            this.btntriangulo.Click += new System.EventHandler(this.btntriangulo_Click);
+            // 
+            // txtTamanoPincel
+            // 
+            this.txtTamanoPincel.Location = new System.Drawing.Point(171, 213);
+            this.txtTamanoPincel.Name = "txtTamanoPincel";
+            this.txtTamanoPincel.Size = new System.Drawing.Size(100, 20);
+            this.txtTamanoPincel.TabIndex = 15;
+            this.txtTamanoPincel.Text = "0";
+            // 
+            // btnCargarImagen
+            // 
+            this.btnCargarImagen.Location = new System.Drawing.Point(259, 272);
+            this.btnCargarImagen.Name = "btnCargarImagen";
+            this.btnCargarImagen.Size = new System.Drawing.Size(191, 23);
+            this.btnCargarImagen.TabIndex = 16;
+            this.btnCargarImagen.Text = "Seleccionar imagen";
+            this.btnCargarImagen.UseVisualStyleBackColor = true;
+            // 
+            // btnRellenado
+            // 
+            this.btnRellenado.Location = new System.Drawing.Point(56, 149);
+            this.btnRellenado.Name = "btnRellenado";
+            this.btnRellenado.Size = new System.Drawing.Size(63, 23);
+            this.btnRellenado.TabIndex = 17;
+            this.btnRellenado.Text = "Rellenar";
+            this.btnRellenado.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.ClientSize = new System.Drawing.Size(1441, 626);
+            this.Controls.Add(this.btnRellenado);
+            this.Controls.Add(this.btnCargarImagen);
+            this.Controls.Add(this.txtTamanoPincel);
+            this.Controls.Add(this.btntriangulo);
+            this.Controls.Add(this.btncuadrado);
+            this.Controls.Add(this.btncirculo);
+            this.Controls.Add(this.txtB);
+            this.Controls.Add(this.txtR);
+            this.Controls.Add(this.txtG);
+            this.Controls.Add(this.trackBartampincel);
+            this.Controls.Add(this.btnBorrar);
+            this.Controls.Add(this.btnPincel);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.picBox);
+            this.Controls.Add(this.picBlack);
+            this.Controls.Add(this.lienzo);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.lienzo)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBlack)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBartampincel)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox lienzo;
+        private System.Windows.Forms.PictureBox picBlack;
+        private System.Windows.Forms.PictureBox picBox;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnPincel;
+        private System.Windows.Forms.Button btnBorrar;
+        private System.Windows.Forms.TrackBar trackBartampincel;
+        private System.Windows.Forms.TextBox txtG;
+        private System.Windows.Forms.TextBox txtR;
+        private System.Windows.Forms.TextBox txtB;
+        private System.Windows.Forms.Button btncirculo;
+        private System.Windows.Forms.Button btncuadrado;
+        private System.Windows.Forms.Button btntriangulo;
+        private System.Windows.Forms.TextBox txtTamanoPincel;
+        private System.Windows.Forms.Button btnCargarImagen;
+        private System.Windows.Forms.Button btnRellenado;
+    }
+}
+
---
+++ Tarea/Form1.cs
@@ -0,0 +1,278 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tarea
+{
+    public partial class Form1 : Form
+    {
+        Graphics papel;
+        int x = 0;
+        int y = 0;
+        int R = 0;
+        int G = 0;
+        int B = 0;
+        int tampincel = 3;
+        bool moviendo = false;
+        Pen pen;
+        bool pintar = false;
+        bool borrar = false;
+        FormaGeometrica formaActual = FormaGeometrica.Ninguna;
+        Color colorFiguras = Color.Black;
+        Pen penFiguras;
+        Color colorRelleno = Color.White;
+        public Form1()
+        {
+            InitializeComponent();
+            penFiguras = new Pen(colorFiguras, 2);
+
+            lienzo.Image = new Bitmap(lienzo.Height, lienzo.Width);
+            papel = lienzo.CreateGraphics();
+            papel.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+            tampincel = trackBartampincel.Value;
+            R = int.Parse(txtR.Text);
+            G = int.Parse(txtG.Text);
+            B = int.Parse(txtB.Text);
+            pen = new Pen(Color.FromArgb(R, G, B), tampincel);
+            pen.StartCap = pen.EndCap = System.Drawing.Drawing2D.LineCap.Round;
+            trackBartampincel.Scroll += trackBartampincel_Scroll;
+            btnCargarImagen.Click += BtnCargarImagen_Click;
+            
+          
+
+        }
+
+
+
+        private void picBlack_Click(object sender, EventArgs e)
+        {
+            txtR.Text = 0.ToString();
+            txtG.Text = 0.ToString();
+            txtB.Text = 0.ToString();
+        }
+
+
+
+        private void lienzo_MouseDown(object sender, MouseEventArgs e)
+        {
+
+            moviendo = true;
+            x = e.X;
+            y = e.Y;
+            lienzo.Cursor = Cursors.Cross;
+            if (pintar)
+            {
+                lienzo.Cursor = Cursors.Default;
+            }
+            else if (formaActual != FormaGeometrica.Ninguna)
+            {
+                x = e.X;
+                y = e.Y;
+                lienzo.Cursor = Cursors.Cross;
+            }
+        }
+
+        private void lienzo_MouseUp(object sender, MouseEventArgs e)
+        {
+            moviendo = false;
+            if (formaActual != FormaGeometrica.Ninguna && !pintar)
+            {
+                moviendo = false;
+                switch (formaActual)
+                {
+                    case FormaGeometrica.Cuadrado:
+                        DibujarForma(formaActual, new Point(x, y), e.Location);
+                        break;
+                    case FormaGeometrica.Circulo:
+                        DibujarForma(formaActual, new Point(x, y), e.Location);
+                        break;
+                    case FormaGeometrica.Triangulo:
+                        
+                        Point punto1 = new Point(x, y);
+                        Point punto2 = new Point(e.X, y);
+                        Point punto3 = new Point((x + e.X) / 2, e.Y);
+                       
+                        DibujarTriangulo(punto1, punto2, punto3);
+                        break;
+                }
+            }
+        }
+
+        private void lienzo_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (moviendo && pintar)
+            {
+                cambiarpincel(int.Parse(txtR.Text), int.Parse(txtG.Text), int.Parse(txtB.Text));
+                papel.DrawLine(pen, new Point(x, y), e.Location);
+                x = e.X; y = e.Y;
+            }
+            if (moviendo && borrar)
+            {
+
+                cambiarpincel(255, 255, 255);
+                papel.DrawLine(pen, new Point(x, y), e.Location);
+                x = e.X; y = e.Y;
+
+            }
+
+        }
+        private void cambiarpincel(int R, int G, int B)
+        {
+            pen = new Pen(Color.FromArgb(R, G, B), trackBartampincel.Value);
+            pen.StartCap = pen.EndCap = System.Drawing.Drawing2D.LineCap.Round;
+        }
+
+        private void picBox_Click(object sender, EventArgs e)
+        {
+            if
+        (colorDialog1.ShowDialog() == DialogResult.OK)
+            {
+                colorFiguras = colorDialog1.Color;
+                penFiguras.Color = colorFiguras;
+                txtR.Text = colorDialog1.Color.R.ToString();
+                txtG.Text = colorDialog1.Color.G.ToString();
+                txtB.Text = colorDialog1.Color.B.ToString();
+                
+            }
+        }
+
+        private void lienzo_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnBorrar_Click(object sender, EventArgs e)
+        {
+            pintar = false;
+            borrar = true;
+            if (!pintar && !borrar)
+            {
+                formaActual = FormaGeometrica.Ninguna;
+            }
+        }
+
+        private void btnPincel_Click(object sender, EventArgs e)
+        {
+            pintar = true;
+            borrar = false;
+            if (!pintar && !borrar)
+            {
+                formaActual = FormaGeometrica.Ninguna;
+            }
+        }
+
+        private void btncuadrado_Click(object sender, EventArgs e)
+        {
+            formaActual = FormaGeometrica.Cuadrado;
+
+        }
+
+        private void btncirculo_Click(object sender, EventArgs e)
+        {
+            formaActual = FormaGeometrica.Circulo;
+        }
+
+        private void btntriangulo_Click(object sender, EventArgs e)
+        {
+            formaActual = FormaGeometrica.Triangulo;
+        }
+        private void DibujarForma(FormaGeometrica forma, Point puntoInicial, Point puntoFinal)
+        {
+
+            Graphics g = lienzo.CreateGraphics();
+            Pen pen = new Pen(colorFiguras, 2); 
+            Brush brush = new SolidBrush(colorFiguras); 
+
+            switch (forma)
+            {
+                case FormaGeometrica.Cuadrado:
+                    int anchoCuadrado = Math.Abs(puntoFinal.X - puntoInicial.X);
+                    int altoCuadrado = Math.Abs(puntoFinal.Y - puntoInicial.Y);
+                    Rectangle rectanguloCuadrado = new Rectangle(puntoInicial.X, puntoInicial.Y, anchoCuadrado, altoCuadrado);
+                    g.DrawRectangle(pen, rectanguloCuadrado);
+                    break;
+                case FormaGeometrica.Circulo:
+                    int radio = Math.Max(Math.Abs(puntoFinal.X - puntoInicial.X), Math.Abs(puntoFinal.Y - puntoInicial.Y));
+                    Rectangle rectanguloCirculo = new Rectangle(puntoInicial.X - radio, puntoInicial.Y - radio, radio * 2, radio * 2);
+                    g.DrawEllipse(pen, rectanguloCirculo);
+                    break;
+                case FormaGeometrica.Triangulo:
+                    // Implementa el dibujo del triángulo aquí
+                    break;
+            }
+
+            g.Dispose();
+        }
+        public enum FormaGeometrica
+        {
+            Ninguna,
+            Cuadrado,
+            Circulo,
+            Triangulo
+        }
+
+        private void DibujarTriangulo(Point punto1, Point punto2, Point punto3)
+        {
+            // Obtener el gráfico del lienzo
+            Graphics g = lienzo.CreateGraphics();
+     
+
+            Point[] puntos = { punto1, punto2, punto3 };
+
+            // Dibujar el triángulo
+            g.DrawPolygon(penFiguras, puntos);
+
+            g.Dispose();
+        }
+        private void trackBartampincel_Scroll(object sender, EventArgs e)
+        {
+            // Actualizar el valor del tamaño del pincel en el TextBox
+            txtTamanoPincel.Text = trackBartampincel.Value.ToString();
+
+            // Aplicar el nuevo tamaño del pincel
+            pen.Width = trackBartampincel.Value;
+        }
+        private void BtnCargarImagen_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                // Establecer el filtro de archivos
+                openFileDialog.Filter = "Archivos de imagen|*.jpg;*.jpeg;*.png;*.bmp|Todos los archivos|*.*";
+                openFileDialog.RestoreDirectory = true;
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    try
+                    {
+                        // Cargar la imagen seleccionada
+                        Image imagen = Image.FromFile(openFileDialog.FileName);
+
+                        // Ajustar el tamaño de la imagen para que se ajuste al PictureBox
+                        lienzo.Image = new Bitmap(lienzo.Width, lienzo.Height);
+                        Graphics g = Graphics.FromImage(lienzo.Image);
+                        g.DrawImage(imagen, new Rectangle(0, 0, lienzo.Width, lienzo.Height));
+
+                        // Liberar recursos
+                        g.Dispose();
+                        imagen.Dispose();
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Error al cargar la imagen: " + ex.Message);
+                    }
+                }
+            }
+        }
+
+    } 
+        
+        }
+    
+
